a8f9bcd33fccc62e2edff24f375985d67bacd062
RAMA PRASAD <link2prasad@gmail.com>
2021-02-20 20:00:08 +0530

    added create user method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,21 @@ class Api::V1::UsersController < ApplicationController
   def show
     render json: User.find(params[:id])
   end
+
+  #POST /users
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created
+    else
+      render_error(user.errors)
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
 end
