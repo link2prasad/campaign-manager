370c10e7d0bf5a1742db47b1709ee8529baeda3e
RAMA PRASAD <link2prasad@gmail.com>
2021-02-20 21:02:06 +0530

    update action added to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
   #GET /users/:id
   def show
-    render json: User.find(params[:id])
+    render json: @user
   end
 
   #POST /users
@@ -14,8 +15,21 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  #PATCH/PUT /users/:id
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render_error(@user.errors)
+    end
+  end
+
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :username)
