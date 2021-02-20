08aba9dfc3aece6b2e8f0bec5dca52de311b6c5e
RAMA PRASAD <link2prasad@gmail.com>
2021-02-20 19:28:36 +0530

    added show to users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_not_found
+
+  private
+  def render_error(msg)
+
+    render :json => {
+        status: 'error',
+        data: {},
+        errors: msg
+    }, status: 422
+    # throw :halt, yield
+    # raise "error"
+    # throw(:abort)
+    # raise Exception.new(msg)
+    # redirect_back(fallback_location: root_path)
+  end
+  def record_not_found(exception)
+    render_error(exception.message)
+    Rails.logger.debug exception.message
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }  do
     namespace :v1 do
       # Our resources will be listed here
+      resources :users, only: [:show]
     end
   end
 end
