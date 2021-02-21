5a96fc8c657b5025e2f91fc35491bb5b44108efa
RAMA PRASAD <link2prasad@gmail.com>
2021-02-21 11:31:27 +0530

    Added authenticable module to manage authentication methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
 
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActiveRecord::RecordInvalid, with: :record_not_found
+  include Authenticable
 
   private
   def render_error(msg)
