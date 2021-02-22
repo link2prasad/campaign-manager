327a1ac855b7dfc7e8f1c1c04d76ff9a3ff33da4
RAMA PRASAD <link2prasad@gmail.com>
2021-02-22 12:05:52 +0530

    added validations to campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,21 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+
+  validates :title,     presence: true
+  validates :purpose,   presence: true
+  validates_presence_of :starts_on, :ends_on
+  validate :end_date_is_after_start_date
+
+
+  private
+
+  def end_date_is_after_start_date
+    return if ends_on.blank? || starts_on.blank?
+    if ends_on < starts_on
+      errors.add(:ends_on, "cannot be before the start date")
+    elsif ((ends_on - starts_on).to_i/1.day) > 90
+      errors.add(:ends_on, "cannot be more than 3 months from the start date")
+    end
+  end
+
 end
--- test/models/campaign_test.rb
+++ test/models/campaign_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class CampaignTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have a valid title" do
+    campaign = campaigns(:one)
+    campaign.title = nil
+    assert_not campaign.valid?
+  end
+
+  test "should have a valid purpose" do
+    campaign = campaigns(:one)
+    campaign.purpose = nil
+    assert_not campaign.valid?
+  end
+
+  test "should have a valid start date" do
+    campaign = campaigns(:one)
+    campaign.starts_on = nil
+    assert_not campaign.valid?
+  end
+
+  test "should have a valid end date" do
+    campaign = campaigns(:one)
+    campaign.ends_on = nil
+    assert_not campaign.valid?
+  end
+
+  test "end date cant be more than 3 months from  start date" do
+    campaign = campaigns(:one)
+    campaign.starts_on = "2021-02-21 16:16:05"
+    campaign.ends_on = "2021-05-23 16:16:05"
+    assert_not campaign.valid?
+  end
+
 end
